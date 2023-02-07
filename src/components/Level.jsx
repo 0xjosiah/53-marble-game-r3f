f02c4fd64df5105b20f1212481cd994a77689d93
Josiah Webb <jj.webb520@gmail.com>
2023-02-07 14:46:44 -0700

    sends pos prop on start block to group

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,11 +1,11 @@
 function StartBlock({ position = [ 0, 0, 0 ] }) {
     return (
-        <>
-            <mesh position={ position } receiveShadow>
+        <group position={ position }>
+            <mesh position={[ 0, -0.1, 0 ]} receiveShadow>
                 <boxGeometry args={[ 4, 0.2, 4 ]} />
                 <meshStandardMaterial color="limegreen" />
             </mesh>
-        </>
+        </group>
     )
 }
 
