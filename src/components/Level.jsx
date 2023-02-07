0c522e67e451196de3f2c8560e78f0406c8c7684
Josiah Webb <jj.webb520@gmail.com>
2023-02-07 15:20:39 -0700

    creates 4 materials to be used in scene

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,9 +1,16 @@
 import * as THREE from 'three'
 
+// general geometry for all boxes in scene
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 
+// Materials for scene
+const floor1Mat = new THREE.MeshStandardMaterial({ color: 'limegreen' })
+const floor2Mat = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
+const obstacleMat = new THREE.MeshStandardMaterial({ color: 'orangered' })
+const wallMat = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+
 /** 
- * this is the inital start block of the game
+ * this is the initial start block of the game
  * @param position = vec3
  * 
 */
