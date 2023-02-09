b4890e44a485671e7cec741b5dbf36ad9733c4bb
Josiah Webb <jj.webb520@gmail.com>
2023-02-09 14:07:00 -0700

    creates axe trap block from limbo duplicate

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -142,12 +142,67 @@ function LimboTrapBlock({ position = [ 0, 0, 0 ] }) {
     )
 }
 
+/**
+ * Trap block with axe style obstacle
+ * @param position = vec3
+ */
+function AxeTrapBlock({ position = [ 0, 0, 0 ] }) {
+    const obstacle = useRef(null)
+
+    // creates constant random value for each instance of this component
+    const [ timeOffset ] = useState(() => (Math.random() * Math.PI * 2))
+
+    // this creates rotation for the rigid body of spinner
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime() + timeOffset
+
+        // ensures limbo trap stays consistent with component position
+        const x = position[0]
+        const y = position[1] + (Math.sin(time) + 1.15)
+        const z = position[2]
+
+        obstacle.current.setNextKinematicTranslation({ x, y, z })
+    })
+
+    return (
+        <group position={ position }>
+            
+            {/* floor */}
+            <mesh
+                geometry={ boxGeometry }
+                material={ floor2Mat }
+                position={[ 0, -0.1, 0 ]}
+                scale={[ 4, 0.2, 4]}
+                receiveShadow
+            />
+
+            {/* obstacle */}
+            <RigidBody
+                ref={ obstacle }
+                type='kinematicPosition'
+                position={[ 0, .3, 0 ]}
+                restitution={ 0.2 }
+                friction={ 0 }
+            >
+                <mesh
+                    geometry={ boxGeometry }
+                    material={ obstacleMat }
+                    scale={[ 3.5, .3, .3 ]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    )
+}
+
 export default function Level(props) {
     return (
         <>
-            <StartBlock position={[ 0, 0, 8 ]} />
-            <SpinnerTrapBlock position={[ 0, 0, 4 ]} />
-            <LimboTrapBlock position={[ 0, 0, 0 ]} />
+            <StartBlock position={[ 0, 0, 12 ]} />
+            <SpinnerTrapBlock position={[ 0, 0, 8 ]} />
+            <LimboTrapBlock position={[ 0, 0, 4 ]} />
+            <AxeTrapBlock position={[ 0, 0, 0 ]} />
         </>
     )
 }
