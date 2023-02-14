2f0f98d355b7d8cea1e0f134cf7f07de81d445fb
Josiah Webb <jj.webb520@gmail.com>
2023-02-13 17:02:44 -0700

    uses blocks memo to render trap blocks

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -327,10 +327,11 @@ function Level({ count = 5, types = [ SpinnerTrapBlock, LimboTrapBlock, SlidingD
 
         return blocks
     }, [ count, types ])
-
+    
     return (
         <>
             <StartBlock position={[ 0, 0, 0 ]} />
+            { blocks.map((Block, index) => <Block position={[ 0, 0, index * 4 ]} /> ) }
         </>
     )
 }
