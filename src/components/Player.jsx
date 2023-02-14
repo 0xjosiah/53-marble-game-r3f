3c02c196e72156d76d859a1fd1a47af2d759acdc
Josiah Webb <jj.webb520@gmail.com>
2023-02-14 15:04:29 -0700

    applies damping to rigid body, adjusts impluse and torque strength multiplier

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,24 +13,24 @@ export default function Player({  }) {
         const impulse = { x: 0, y: 0, z: 0 }
         const torque = { x: 0, y: 0, z: 0 }
 
-        const impulseStrength = delta * 1
-        const torqueStrength = delta * 1
+        const impulseStrength = delta * 0.6
+        const torqueStrength = delta * 0.2
 
         if(forward) {
             impulse.z -= impulseStrength
             torque.x -= torqueStrength
         }
-        
+    
         if(starboard) {
             impulse.x += impulseStrength
             torque.z -= torqueStrength
         }
-        
+
         if(reverse) {
             impulse.z += impulseStrength
             torque.x += torqueStrength
         }
-        
+
         if(port) {
             impulse.x -= impulseStrength
             torque.z += torqueStrength
@@ -47,6 +47,8 @@ export default function Player({  }) {
             position={[ 0, 1, 0 ]}
             restitution={ 0.2 }
             friction={ 1 }
+            linearDamping={ 0.5 }
+            angularDamping={ 0.5 }
         >
             <mesh castShadow>
                 <icosahedronGeometry args={[ 0.3, 1 ]} />
