bf577e5b99c49ebbdc27f73ee802b06823f2f023
Josiah Webb <jj.webb520@gmail.com>
2023-02-16 12:34:57 -0700

    adds interface comp to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './components/Experience.jsx'
 import { KeyboardControls } from '@react-three/drei'
+import Interface from './components/Interface'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -27,6 +28,7 @@ root.render(
             } }
         >
             <Experience />
+            <Interface />
         </Canvas>
     </KeyboardControls>
 )
