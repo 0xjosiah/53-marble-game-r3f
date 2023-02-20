5379101c2d52b904d6cc1325ca69ac58a3a2b18d
Josiah Webb <jj.webb520@gmail.com>
2023-02-20 13:06:35 -0700

    adds startTime and endTime to useGame

--- src/stores/useGame.js
+++ src/stores/useGame.js
@@ -4,14 +4,22 @@ import { subscribeWithSelector } from 'zustand/middleware'
 export default create(subscribeWithSelector((set) => ({
     blocksCount: 3,
 
+    /* Time */
+    startTime: 0,
+    endTime: 0,
+
     /* Phases */
     phase: 'ready',
     
     start: () => {
         set((state) => {
             // if phase is already playing, don't want to update anything; this prevents start from firing over and over
-            if(state.phase === 'ready')
-                return { phase: 'playing' }
+            if(state.phase === 'ready') {
+                return {
+                    phase: 'playing',
+                    startTime: Date.now()
+                }
+            }
             // must return something from this fn    
             return {}
         })
@@ -29,7 +37,10 @@ export default create(subscribeWithSelector((set) => ({
     end: () => {
         set((state) => {
             if(state.phase === 'playing') {
-                return { phase: 'ended'}
+                return {
+                    phase: 'ended',
+                    endTime: Date.now()
+                }
             }
             return {}
         })
